c4e7e4acc7779fbf8bf78020ee882b62f2e792e8
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-03-26 23:33:17 +1100

    refactor: Directory change

--- media/lua/client/AQTranslations.lua
+++ media/lua/client/AQTranslations.lua
@@ -15,13 +15,15 @@ AQTranslations.ContextMenu_AQBrushTeeth = "nil"
 AQTranslations.IGUI_AQBrushTeeth_WithoutToothpaste = "nil"
 AQTranslations.IGUI_AQBrushTeeth_Toothpaste = "nil"
 
-function AQTranslations:updateCache()
+function AQTranslations.update()
     -- Vanilla
-    self.ContextMenu_WaterName = getText("ContextMenu_WaterName")
+    AQTranslations.ContextMenu_WaterName = getText("ContextMenu_WaterName")
     -- Modded
-    self.ContextMenu_AQBrushTeeth = getText("ContextMenu_AQBrushTeeth")
-    self.IGUI_AQBrushTeeth_WithoutToothpaste = getText("IGUI_AQBrushTeeth_WithoutToothpaste")
-    self.IGUI_AQBrushTeeth_Toothpaste = getText("IGUI_AQBrushTeeth_Toothpaste")
+    AQTranslations.ContextMenu_AQBrushTeeth = getText("ContextMenu_AQBrushTeeth")
+    AQTranslations.ContextMenu_AQBrushTeeth_AlreadyBrushed = getText("ContextMenu_AQBrushTeeth_AlreadyBrushed")
+    AQTranslations.ContextMenu_AQBrushTeeth_TooDepressed = getText("ContextMenu_AQBrushTeeth_TooDepressed")
+    AQTranslations.IGUI_AQBrushTeeth_WithoutToothpaste = getText("IGUI_AQBrushTeeth_WithoutToothpaste")
+    AQTranslations.IGUI_AQBrushTeeth_Toothpaste = getText("IGUI_AQBrushTeeth_Toothpaste")
 end
 
 return AQTranslations
