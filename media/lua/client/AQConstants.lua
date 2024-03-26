45b72f69e730cda28bc1814c76d202b949779fb1
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-03-26 23:09:22 +1100

    feat(constants): Added mod version

--- media/lua/client/AQConstants.lua
+++ media/lua/client/AQConstants.lua
@@ -10,5 +10,6 @@ AQConstants.IS_DEBUG = getDebug()
 AQConstants.IS_LAST_STAND = getCore():getGameMode() == "LastStand"
 
 AQConstants.MOD_ID = "aoqia-toothbrush-mod"
+AQConstants.MOD_VERSION = "1.0.0"
 
 return AQConstants
