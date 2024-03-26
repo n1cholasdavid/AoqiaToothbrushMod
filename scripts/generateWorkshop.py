b57aeb2b61d374e259a260272d12e61ddd1a7e35
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-03-27 04:17:51 +1100

    feat(script): Cool new workshop script
    Used to generate the distribution files for Steam workshop. This is because everyone downloading the mod on the Steam workshop does not need repository files.

---
+++ scripts/generateWorkshop.py
@@ -0,0 +1,41 @@
+import fnmatch
+import os
+import shutil
+
+SRC = "../"
+DEST = "../dist/"
+
+EXCLUDE_PATTERNS = [
+    "*.py",
+    "*.code-workspace",
+    ".gitignore",
+    ".luarc.json",
+    "TODO.md",
+    "README.md",
+    "CHANGELOG.md",
+]
+EXCLUDED_DIRS = [".git", ".vscode", "dist", "scripts"]
+
+print("Generating workshop files...")
+
+if not os.path.exists(DEST):
+    os.makedirs(DEST)
+
+for dirpath, dirnames, filenames in os.walk(SRC):
+    for excluded_dir in EXCLUDED_DIRS:
+        if excluded_dir in dirpath:
+            print("Matched directory\t", excluded_dir)
+            break
+    else:  # No excluded directory found
+        for filename in filenames:
+            for pattern in EXCLUDE_PATTERNS:
+                if fnmatch.fnmatch(filename, pattern):
+                    print("Matched pattern\t", pattern)
+                    break
+            else:
+                src_file = os.path.join(dirpath, filename)
+                dest_file = os.path.join(DEST, src_file[3:])
+                os.makedirs(os.path.dirname(dest_file), exist_ok=True)
+                shutil.copy2(src_file, dest_file)
+
+print("Workshop files generated successfully.")
