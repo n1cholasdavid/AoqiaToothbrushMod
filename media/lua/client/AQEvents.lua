f7f24a58c40460389223439b58c70051f2080adb
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-04-04 17:12:57 +1100

    fix(moddata): Should contain mod version not mod id
    refactor: Merge AQModData file with AQEvents file.

--- media/lua/client/AQEvents.lua
+++ media/lua/client/AQEvents.lua
@@ -17,6 +17,17 @@ local AQTweaks                 = require("AQTweaks")
 
 local AQEvents                 = {}
 
+-- Only used for type-checking purposes in ModData.
+---@class AQModDataStruct
+---@field _modVersion string | nil
+---@field daysWithoutBrushingTeeth number | nil
+---@field timesBrushedTeethToday number | nil
+local AQModDataStructDummy     = {
+    _modVersion = AQConstants.MOD_VERSION,
+    daysWithoutBrushingTeeth = 0,
+    timesBrushedTeethToday = 0,
+}
+
 function AQEvents.register()
     Events.OnGameBoot.Add(function ()
         AQTranslations.update()
@@ -27,18 +38,18 @@ function AQEvents.register()
         -- I am doing this seperately because I think creating and returning a table that...
         -- also internally checks if the table exists before creating it will cost some performance.
         if newGame then
-            ModData.add(AQConstants.MOD_ID, AQModData.AQModDataStructDummy)
+            ModData.add(AQConstants.MOD_ID, AQModDataStructDummy)
         else
             -- If it doesn't exist then create new, otherwise load it and check nil. If nil then create new.
             if not ModData.exists(AQConstants.MOD_ID) then
-                ModData.add(AQConstants.MOD_ID, AQModData.AQModDataStructDummy)
+                ModData.add(AQConstants.MOD_ID, AQModDataStructDummy)
             else
                 ---@type AQModDataStruct
                 local data = ModData.get(AQConstants.MOD_ID)
                 if data.daysWithoutBrushingTeeth == nil
                 or data.timesBrushedTeethToday == nil
-                or data._modId == nil then
-                    ModData.add(AQConstants.MOD_ID, AQModData.AQModDataStructDummy)
+                or data._modVersion == nil then
+                    ModData.add(AQConstants.MOD_ID, AQModDataStructDummy)
                 end
             end
         end
@@ -47,7 +58,7 @@ function AQEvents.register()
     Events.OnPlayerDeath.Add(function (deadPlayer)
         local clientPlayer = getPlayer()
         if deadPlayer == clientPlayer then
-            ModData.add(AQConstants.MOD_ID, AQModData.AQModDataStructDummy)
+            ModData.add(AQConstants.MOD_ID, AQModDataStructDummy)
         end
     end)
 
