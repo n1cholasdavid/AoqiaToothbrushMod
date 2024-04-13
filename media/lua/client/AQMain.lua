d987f6427746be53a8e88598015536dcfc64a9c6
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-04-14 06:49:43 +1000

    feat(mods): Check for active mods
    Check for active mods before registering events.

--- media/lua/client/AQMain.lua
+++ media/lua/client/AQMain.lua
@@ -2,11 +2,18 @@
 --                      The main entry point for the mod.                     --
 -- -------------------------------------------------------------------------- --
 
+local getActivatedMods = getActivatedMods
+
 local AQEvents = require("AQEvents")
 local AQUtils = require("AQUtils")
 
 -- ------------------------------- Entrypoint ------------------------------- --
 
-AQEvents.register()
+local activated_mods = getActivatedMods()
+if not activated_mods:contains("ItemTweakerAPI") or not activated_mods:contains("MoodleFramework") then
+    AQUtils.logerror("Failed to find ItemTweakerAPI or MoodleFramework.")
+    return
+end
 
+AQEvents.register()
 AQUtils.logdebug("Up and running!")
