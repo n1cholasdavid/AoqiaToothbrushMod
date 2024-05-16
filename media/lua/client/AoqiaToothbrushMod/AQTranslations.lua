dbda420fec6dd98c428b832d6af15ac194098368
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-05-16 19:24:04 +1000

    feat(translations): Add keys

--- media/lua/client/AoqiaToothbrushMod/AQTranslations.lua
+++ media/lua/client/AoqiaToothbrushMod/AQTranslations.lua
@@ -2,43 +2,67 @@
 --   Caches translations so that we don't call `getText()` a billion times.   --
 -- -------------------------------------------------------------------------- --
 
-local getText = getText
+local getText                             = getText
 
-local AQConstants = require("AoqiaToothbrushMod/AQConstants")
+local AQConstants                         = require("AoqiaToothbrushMod/AQConstants")
+local AQLog = require("AoqiaToothbrushMod/AQLog")
 
 -- ------------------------------ Module Start ------------------------------ --
 
-local AQTranslations = {}
+local AQTranslations                      = {}
 
 -- Only really for autocomplete.
 -- Vanilla
-AQTranslations.ContextMenu_WaterName = nil
+AQTranslations.ContextMenu_WaterName      = nil
 -- Modded
-AQTranslations.ContextMenu_BrushTeeth = nil
+AQTranslations.ContextMenu_BrushTeeth     = nil
 AQTranslations.ContextMenu_AlreadyBrushed = nil
-AQTranslations.ContextMenu_TooDepressed = nil
-AQTranslations.IGUI_WithoutToothpaste = nil
-AQTranslations.IGUI_Toothpaste = nil
-AQTranslations.Sandbox_Test = nil
-AQTranslations.Tooltip_Toothbrush = nil
-AQTranslations.Tooltip_Toothpaste = nil
+AQTranslations.ContextMenu_TooDepressed   = nil
+AQTranslations.IGUI_WithoutToothpaste     = nil
+AQTranslations.IGUI_Toothpaste            = nil
+AQTranslations.Sandbox_Test               = nil
+AQTranslations.Tooltip_Toothbrush         = nil
+AQTranslations.Tooltip_Toothpaste         = nil
+AQTranslations.UI_GoldenBrusherTrait      = nil
+AQTranslations.UI_GoldenBrusherTraitDesc  = nil
+AQTranslations.UI_FoulBrusherTrait        = nil
+AQTranslations.UI_FoulBrusherTraitDesc    = nil
 
-function AQTranslations.update()
+-- Translation Keys
+-- Vanilla
+AQTranslations.ContextMenu_WaterName_key      = "ContextMenu_WaterName"
+-- Modded
+AQTranslations.ContextMenu_BrushTeeth_key     = ("ContextMenu_" .. AQConstants.MOD_ID .. "_BrushTeeth")
+AQTranslations.ContextMenu_AlreadyBrushed_key = ("ContextMenu_" .. AQConstants.MOD_ID .. "_AlreadyBrushed")
+AQTranslations.ContextMenu_TooDepressed_key   = ("ContextMenu_" .. AQConstants.MOD_ID .. "_TooDepressed")
+AQTranslations.IGUI_WithoutToothpaste_key     = ("IGUI_" .. AQConstants.MOD_ID .. "_WithoutToothpaste")
+AQTranslations.IGUI_Toothpaste_key            = ("IGUI_" .. AQConstants.MOD_ID .. "_Toothpaste")
+AQTranslations.Sandbox_Test_key               = ("Sandbox_" .. AQConstants.MOD_ID .. "_Test")
+AQTranslations.Tooltip_Toothbrush_key         = ("Tooltip_" .. AQConstants.MOD_ID .. "_Toothbrush")
+AQTranslations.Tooltip_Toothpaste_key         = ("Tooltip_" .. AQConstants.MOD_ID .. "_Toothpaste")
+AQTranslations.UI_GoldenBrusherTrait_key      = ("UI_" .. AQConstants.MOD_ID .. "_GoldenBrusherTrait")
+AQTranslations.UI_GoldenBrusherTraitDesc_key  = ("UI_" .. AQConstants.MOD_ID .. "_GoldenBrusherTraitDesc")
+AQTranslations.UI_FoulBrusherTrait_key        = ("UI_" .. AQConstants.MOD_ID .. "_FoulBrusherTrait")
+AQTranslations.UI_FoulBrusherTraitDesc_key    = ("UI_" .. AQConstants.MOD_ID .. "_FoulBrusherTraitDesc")
+
+function AQTranslations.init()
+    AQLog.debug("Caching translations...")
+
     -- Vanilla
-    AQTranslations.ContextMenu_WaterName = getText("ContextMenu_WaterName")
+    AQTranslations.ContextMenu_WaterName = getText(AQTranslations.ContextMenu_WaterName_key)
     -- Modded
-    AQTranslations.ContextMenu_BrushTeeth = getText("ContextMenu_" .. AQConstants.MOD_ID .. "_BrushTeeth")
-    AQTranslations.ContextMenu_AlreadyBrushed = getText("ContextMenu_" .. AQConstants.MOD_ID .. "_AlreadyBrushed")
-    AQTranslations.ContextMenu_TooDepressed = getText("ContextMenu_" .. AQConstants.MOD_ID .. "_TooDepressed")
-    AQTranslations.IGUI_WithoutToothpaste = getText("IGUI_" .. AQConstants.MOD_ID .. "_WithoutToothpaste")
-    AQTranslations.IGUI_Toothpaste = getText("IGUI_" .. AQConstants.MOD_ID .. "_Toothpaste")
-    AQTranslations.Sandbox_Test = getText("Sandbox_" .. AQConstants.MOD_ID .. "_Test")
-    AQTranslations.Tooltip_Toothbrush = getText("Tooltip_" .. AQConstants.MOD_ID .. "_Toothbrush")
-    AQTranslations.Tooltip_Toothpaste = getText("Tooltip_" .. AQConstants.MOD_ID .. "_Toothpaste")
-    AQTranslations.UI_GoldenBrusherTrait = getText("UI_" .. AQConstants.MOD_ID .. "_GoldenBrusherTrait")
-    AQTranslations.UI_GoldenBrusherTraitDesc = getText("UI_" .. AQConstants.MOD_ID .. "_GoldenBrusherTraitDesc")
-    AQTranslations.UI_FoulBrusherTrait = getText("UI_" .. AQConstants.MOD_ID .. "_FoulBrusherTrait")
-    AQTranslations.UI_FoulBrusherTraitDesc = getText("UI_" .. AQConstants.MOD_ID .. "_FoulBrusherTraitDesc")
+    AQTranslations.ContextMenu_BrushTeeth = getText(AQTranslations.ContextMenu_BrushTeeth_key)
+    AQTranslations.ContextMenu_AlreadyBrushed = getText(AQTranslations.ContextMenu_AlreadyBrushed_key)
+    AQTranslations.ContextMenu_TooDepressed = getText(AQTranslations.ContextMenu_TooDepressed_key)
+    AQTranslations.IGUI_WithoutToothpaste = getText(AQTranslations.IGUI_WithoutToothpaste_key)
+    AQTranslations.IGUI_Toothpaste = getText(AQTranslations.IGUI_Toothpaste_key)
+    AQTranslations.Sandbox_Test = getText(AQTranslations.Sandbox_Test_key)
+    AQTranslations.Tooltip_Toothbrush = getText(AQTranslations.Tooltip_Toothbrush_key)
+    AQTranslations.Tooltip_Toothpaste = getText(AQTranslations.Tooltip_Toothpaste_key)
+    AQTranslations.UI_GoldenBrusherTrait = getText(AQTranslations.UI_GoldenBrusherTrait_key)
+    AQTranslations.UI_GoldenBrusherTraitDesc = getText(AQTranslations.UI_GoldenBrusherTraitDesc_key)
+    AQTranslations.UI_FoulBrusherTrait = getText(AQTranslations.UI_FoulBrusherTrait_key)
+    AQTranslations.UI_FoulBrusherTraitDesc = getText(AQTranslations.UI_FoulBrusherTraitDesc_key)
 end
 
 return AQTranslations
