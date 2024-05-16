e706a6c8bab3ab92df75258f03ce7fe509486103
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-05-16 19:25:28 +1000

    fix(tweaks): Change tweaks

--- media/lua/client/AoqiaToothbrushMod/AQTweaks.lua
+++ media/lua/client/AoqiaToothbrushMod/AQTweaks.lua
@@ -2,36 +2,37 @@
 --                 Registers tweaks using the ItemTweakerAPI.                 --
 -- -------------------------------------------------------------------------- --
 
+local AQLog = require("AoqiaToothbrushMod/AQLog")
 local AQTranslations = require("AoqiaToothbrushMod/AQTranslations")
 
 -- ------------------------------ Module Start ------------------------------ --
 
 local AQTweaks = {}
 
-function AQTweaks.apply()
-    do
-        local TweakItem = TweakItem
-        local tweaks = {
-            ["Base.Toothbrush"] = {
-                ["DisplayCategory"] = "FirstAid",
-                ["Weight"] = "0.05",
-                ["Tooltip"] = AQTranslations.Tooltip_Toothbrush,
-            },
-            ["Base.Toothpaste"] = {
-                ["DisplayCategory"] = "FirstAid",
-                -- TODO: As much as I'd like to do this, I cannot until I figure out how to handle item type change deleting it from world.
-                -- ["Type"] = "Drainable",
-                ["UseDelta"] = "0.05",
-                ["UseWhileEquipped"] = "FALSE",
-                ["cantBeConsolided"] = "TRUE",
-                ["Tooltip"] = AQTranslations.Tooltip_Toothpaste,
-            }
-        }
+function AQTweaks.init()
+    AQLog.debug("Applying tweaks...")
 
-        for item, itemData in pairs(tweaks) do
-            for tweak, tweakValue in pairs(itemData) do
-                TweakItem(item, tweak, tweakValue)
-            end
+    local TweakItem = TweakItem
+    local tweaks = {
+        ["Base.Toothbrush"] = {
+            ["DisplayCategory"] = "FirstAid",
+            ["Weight"] = "0.05",
+            ["Tooltip"] = AQTranslations.Tooltip_Toothbrush_key,
+        },
+        ["Base.Toothpaste"] = {
+            ["DisplayCategory"] = "FirstAid",
+            -- TODO: As much as I'd like to do this, I cannot until I figure out how to handle item type change deleting it from world.
+            -- ["Type"] = "Drainable",
+            ["UseDelta"] = "0.05",
+            ["UseWhileEquipped"] = "FALSE",
+            ["cantBeConsolided"] = "TRUE",
+            ["Tooltip"] = AQTranslations.Tooltip_Toothpaste_key,
+        }
+    }
+
+    for item, itemData in pairs(tweaks) do
+        for tweak, tweakValue in pairs(itemData) do
+            TweakItem(item, tweak, tweakValue)
         end
     end
 end
