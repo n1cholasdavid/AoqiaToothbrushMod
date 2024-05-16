0be0817e32e65d65355626d8c1ec221ba4b8fc9f
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-05-16 19:12:10 +1000

    chore(version): Increment version number

--- media/lua/shared/AoqiaToothbrushMod/AQConstants.lua
+++ media/lua/shared/AoqiaToothbrushMod/AQConstants.lua
@@ -10,6 +10,6 @@ AQConstants.IS_DEBUG = getDebug() == true
 AQConstants.IS_LAST_STAND = getCore():getGameMode() == "LastStand"
 
 AQConstants.MOD_ID = "AoqiaToothbrushMod"
-AQConstants.MOD_VERSION = "1.1.0"
+AQConstants.MOD_VERSION = "1.2.0"
 
 return AQConstants
