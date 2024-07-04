41e14f53e8c21c8d5c74f989e3acd8ae3939fb49
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-07-04 13:53:25 +1000

    feat(static): Add static mod constants

---
+++ media/lua/shared/AoqiaToothbrushMod/mod_constants.lua
@@ -0,0 +1,16 @@
+-- -------------------------------------------------------------------------- --
+--                   Stores constants to be used everywhere.                  --
+-- -------------------------------------------------------------------------- --
+
+local logger = require("AoqiaZomboidUtils/logger")
+
+-- ------------------------------ Module Start ------------------------------ --
+
+local mod_constants = {}
+
+mod_constants.MOD_ID = "AoqiaToothbrushMod"
+mod_constants.MOD_VERSION = "1.2.0"
+
+mod_constants.LOGGER = logger:new(mod_constants.MOD_ID)
+
+return mod_constants
